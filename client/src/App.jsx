392d644bd4d0fc832cd09901f6834b105a2141c6
yvanopara <yvanopara4000@gmail.com>
2024-04-06 02:39:23 +0200

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
 
-const App = () => {
+
+export default function App() {
   return (
-    <div>
-      <p className='text-9xl'>bonjour hi</p>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+   
   )
 }
-
-export default App
 
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignUp() {
+  return (
+    <div>
+      <p>SIGNUP</p>
+    </div>
+  )
+}
