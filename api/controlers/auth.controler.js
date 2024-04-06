31e21090f509bd80844ed49f770424abbfab4a3e
yvanopara <yvanopara4000@gmail.com>
2024-04-06 21:51:50 +0200

    function to handle errors

--- api/controlers/auth.controler.js
+++ api/controlers/auth.controler.js
@@ -1,12 +1,13 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'
 
-export const signup = async (req, res) =>{
+
+export const signup = async (req, res, next) =>{
     // console.log(req.body);
     const {username, password, email} = req.body;
     if(!email|| !username|| !password || email==="" || password==="" || username==="")
     {
-        return res.status(400).json({message:"All fields are required"});
+        next(errorHandler(400, "All fields are required"));
     }
     const hashPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({
@@ -20,6 +21,6 @@ export const signup = async (req, res) =>{
     res.json("signup successful");
     
     }catch(error){
-        res.status(500).json({message:error.message});
+        next(error);
     }
 }
